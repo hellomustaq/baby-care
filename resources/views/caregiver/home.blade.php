c2c9caeb444508b65b17d33d856b52056a00cedd
hellomustaq <hellomstq@gmail.com>
2018-07-22 04:25:32 +0600

    clean caregiver.home

--- resources/views/caregiver/home.blade.php
+++ resources/views/caregiver/home.blade.php
@@ -1,17 +1,5 @@
 @extends('caregiver.layout.master')
 
 @section('content')
-<div class="container">
-    <div class="row">
-        <div class="col-md-8 col-md-offset-2">
-            <div class="panel panel-default">
-                <div class="panel-heading">Dashboard</div>
 
-                <div class="panel-body">
-                    You are logged in as Caregiver!
-                </div>
-            </div>
-        </div>
-    </div>
-</div>
 @endsection
