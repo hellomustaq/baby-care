fb4eaac933f0b9884bfcc067e0c36eb7c3234962
hellomustaq <hellomstq@gmail.com>
2018-07-27 23:06:53 +0600

    delele passing in controller set all ready boot()

--- app/Http/Controllers/CaregiverController.php
+++ app/Http/Controllers/CaregiverController.php
@@ -11,15 +11,7 @@ use Auth;
 class CaregiverController extends Controller
 {
    	public function childrenProfile($cid){
-   		if (Auth::guard('caregiver')->check()) {
-   			$userId=Auth::guard('caregiver')->user()->id;
-   		}else{
-   			die('not log in');
-   		}
-   		
-        $caregiver=Caregiver::find($userId);
-        $childrens=Children::where('caregiver_id','=',$userId)->get();
    		$selected=Children::where('id','=',$cid)->first();
-   		return view('caregiver.childProfile')->with('selected',$selected)->with('childrens',$childrens);
+   		return view('caregiver.childProfile')->with('selected',$selected);
    	}
 }
